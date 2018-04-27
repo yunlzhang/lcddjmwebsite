f2307eaffe9b99382e331c4422d69952b78f6566
yunlzhang <zhangyunlong_fdzs@163.com>
2018-04-27 08:17:22 +0800

    评论

--- server/models/comment.js
+++ server/models/comment.js
@@ -2,9 +2,14 @@ const Post = require('../lib/mongoose').Comment;
 const Promise = require("bluebird");
 
 module.exports = {
-    // 注册一个用户
-    create(data) {
-      let newComment = new comment(data);
-      return newComment.save();
-    }
-  };
+	// 注册一个用户
+	create(data) {
+		let newComment = new comment(data);
+		return newComment.save();
+	},
+	getPostById(id) {
+		return Post.find({
+			'article_id':id,
+		}).sort({_id: 1}).lean();
+	}
+};
